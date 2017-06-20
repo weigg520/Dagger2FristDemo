ea8ff3513d485b79328f8206447dfa065c6a2db6
weigg <752139816@qq.com>
2017-06-20 15:46:35 +0800

    my frist commit

---
+++ app/src/main/java/weizhengzhou/top/dagger2fristdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package weizhengzhou.top.dagger2fristdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
